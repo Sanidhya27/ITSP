e933e21371fae84fc8eb9b708c4d843ac0375d5a
Sanidhya27 <cool.sanidhyagwl@gmail.com>
2017-05-18 14:51:45 +0530

    final

--- pygame.py
+++ pygame.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.font
+import motor control
 pygame.init()
 display=pygame.display.set_mode((500,500))
 pygame.display.set_caption("DIRECTION CONTROL")
@@ -8,5 +9,20 @@ text_position = text.get_rect(centerx=250)
 text = display_font.render('Give Directions Using Arrow Keys', 1,(0,255,255))
 display.blit(text)
 pygame.display.update()
-
-
+try:
+	while True:
+		command=""
+		event=pygame.event.get()
+		if event.type==pygame.KEYDOWN:
+			if event.key==pygame.K_LEFT:
+				command="LEFT"
+			if event.key==pygame.K_RIGHT:
+				command="RIGHT"
+			if event.key==pygame.K_UP:
+				command="FORWARD"
+			if event.key==pygame.K_DOWN:
+				command="BACK"
+		print(command)		
+		instructions(command)	
+except KeyboardInterrupt:
+	pygame.quit()
